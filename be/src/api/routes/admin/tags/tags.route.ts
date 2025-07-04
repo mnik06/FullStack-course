57dc28d0b449c0197908b45aad73d17b0e5d63dd
Maksym Nikitchenko <m.nikitchenko@softonix.org>
2025-07-04 19:13:39 +0300

    feat/be: updated response schemas

--- be/src/api/routes/admin/tags/tags.route.ts
+++ be/src/api/routes/admin/tags/tags.route.ts
@@ -5,11 +5,18 @@ import { requirePermission } from 'src/api/hooks/require-permission.hook';
 import { CreateTagReqSchema } from 'src/api/routes/schemas/admin/tags/CreateTagReqSchema';
 import { createTag } from 'src/controllers/admin/tags/create-tag';
 import { getTags } from 'src/controllers/admin/tags/get-tags';
+import { GetTagsRespSchema } from 'src/api/routes/schemas/admin/tags/GetTagsRespSchema';
+import { GetTagByIdRespSchema } from 'src/api/routes/schemas/admin/tags/GetTagByIdRespSchema';
 
 const routes: FastifyPluginAsync = async function (f) {
   const fastify = f.withTypeProvider<ZodTypeProvider>();
 
   fastify.get('/', {
+    schema: {
+      response: {
+        200: GetTagsRespSchema
+      }
+    },
     preHandler: [requirePermission('manage_tags')]
   }, () => {
     return getTags({
@@ -19,7 +26,10 @@ const routes: FastifyPluginAsync = async function (f) {
 
   fastify.post('/', {
     schema: {
-      body: CreateTagReqSchema
+      body: CreateTagReqSchema,
+      response: {
+        200: GetTagByIdRespSchema
+      }
     },
     preHandler: [requirePermission('manage_tags')]
   }, (req) => {
---
+++ be/src/api/routes/schemas/admin/tags/GetTagByIdRespSchema.ts
@@ -0,0 +1,3 @@
+import { TagSchema } from 'src/types/tag/schemas/Tag';
+
+export const GetTagByIdRespSchema = TagSchema.extend({});
