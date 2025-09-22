8946715c1389dec19680c2de6da435e1f63d8c21
DenysPoliarush <66991536+DenysPoliarush@users.noreply.github.com>
2025-09-22 19:12:44 +0300

    one more comment

--- fe/src/plugins/amplify.ts
+++ fe/src/plugins/amplify.ts
@@ -1,3 +1,4 @@
 import { Amplify } from 'aws-amplify'
 import amplifyconfig from '../amplifyconfiguration.json'
+// CODE REVIEW - ключі до когніто мають бути в .env файлі. Це публічні ключі тому їх можна пушити в гіт
 Amplify.configure(amplifyconfig)
